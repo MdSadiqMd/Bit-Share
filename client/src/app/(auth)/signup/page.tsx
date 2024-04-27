085544e626566c383be7a4e7c9a5d5853f1d898e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-27 20:39:17 +0530

    refactor added signup form with routes

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -34,14 +34,13 @@ const signupForm = () => {
     password: "",
   } as FormData);
   const [isLoading, useIsLoading] = useState<Boolean>(true);
-  const [isLoadingOTP, useIsLoadingOTP] = useState<Boolean>(true);
 
   const handleLoading = () => {
     useIsLoading(!isLoading);
   };
 
   const handleLoadingOTP = () => {
-    useIsLoadingOTP(!isLoadingOTP);
+    setSendingOtp(!sendingOtp);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -51,7 +50,7 @@ const signupForm = () => {
 
   const sendOTP = async () => {
     setSendingOtp(true);
-    let res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/auth/sendotp", {
+    let res = await fetch(process.env.NEXT_PUBLIC_URL + "/auth/sendotp", {
       method: "POST",
       body: JSON.stringify({ email: formData.email }),
       headers: {
@@ -67,82 +66,158 @@ const signupForm = () => {
       toast.error(data.message);
     }
   };
+  const handleSignup = async () => {
+    if (
+      formData.name == "" ||
+      formData.email == "" ||
+      formData.password == "" ||
+      otp == ""
+    ) {
+      toast.error("Please fill all the fields");
+      return;
+    }
+    let formdata = new FormData();
+
+    formdata.append("name", formData.name);
+    formdata.append("email", formData.email);
+    formdata.append("password", formData.password);
+    formdata.append("otp", otp);
+    if (imageFile) {
+      formdata.append("clientfile", imageFile);
+    }
+
+    /*
+    let singupdetails = {
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+      otp: otp,
+      //profilePic: filekey
+    };*/
+    let res = await fetch(process.env.NEXT_PUBLIC_URL + "/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: formdata, //JSON.stringify(singupdetails),
+      credentials: "include",
+    });
+
+    let data = await res.json();
+    if (data.ok) {
+      toast.success("Signup successful");
+      Router.push("/login");
+    } else {
+      toast.error(data.message);
+    }
+  };
 
   return (
-    <Card className="mx-auto max-w-md mt-[1.5%]">
-      <CardHeader>
-        <CardTitle className="text-xl">Sign Up</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4 space-y-2">
-          <div className="grid gap-2">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Enter your name"value={formData.name}
-                onChange={handleInputChange} required />
-          </div>
-          <div className="grid grid-cols-3 gap-3 items-center">
-            <div className="col-span-2 grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="youremail@gmail.com"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-              />
+    <>
+      <Card className="mx-auto max-w-md mt-[1.5%]">
+        <div>
+          <CardHeader> 
+              <CardTitle className="text-xl">Sign Up</CardTitle>
+              <CardDescription>
+                Enter your information to create an account
+              </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4 space-y-2">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  placeholder="Enter your name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="grid grid-cols-3 gap-3 items-center">
+                <div className="col-span-2 grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="youremail@gmail.com"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div className="col-span-1 flex mt-6 items-center justify-center">
+                  {sendingOtp ? (
+                    <Button
+                      onClick={() => {
+                        handleLoadingOTP(), sendOTP;
+                      }}
+                      className="w-30"
+                    >
+                      Send OTP
+                    </Button>
+                  ) : (
+                    <Button disabled>
+                      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                      Please wait
+                    </Button>
+                  )}
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <InputOTPForm
+                  value={otp}
+                  onChange={(e: any) => setOtp(e?.target?.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="image">Your Profile Pic</Label>
+                <Input
+                  id="image"
+                  type="file"
+                  onChange={(e: any) => setImageFile(e.target.files![0])}
+                />
+              </div>
+              <div>
+                {isLoading ? (
+                  <Button
+                    onClick={() => handleLoading()}
+                    type="submit"
+                    className="w-full"
+                  >
+                    Create an Account
+                  </Button>
+                ) : (
+                  <Button disabled>
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                    Please wait
+                  </Button>
+                )}
+              </div>
             </div>
-            <div className="col-span-1 flex mt-6 items-center justify-center">
-              {isLoadingOTP ? (
-                <Button onClick={() => {handleLoadingOTP(),sendOTP}} className="w-30">
-                  Send OTP
-                </Button>
-              ) : (
-                <Button disabled>
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                  Please wait
-                </Button>
-              )}
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="underline"
+                onClick={() => handleSignup()}
+              >
+                Sign in
+              </Link>
             </div>
-          </div>
-          <div className="grid gap-2">
-            <InputOTPForm value={otp} onChange={(e:any)=>setOtp(e?.target?.value)} />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" value={formData.password}
-                onChange={handleInputChange}/>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="image">Your Profile Pic</Label>
-            <Input id="image" type="file"/>
-          </div>
-          {isLoading ? (
-            <Button
-              onClick={() => handleLoading()}
-              type="submit"
-              className="w-full"
-            >
-              Create an Account
-            </Button>
-          ) : (
-            <Button disabled>
-              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-              Please wait
-            </Button>
-          )}
+          </CardContent>
         </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <Link href="/login" className="underline">
-            Sign in
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+      </Card>
+    </>
   );
 };
 
