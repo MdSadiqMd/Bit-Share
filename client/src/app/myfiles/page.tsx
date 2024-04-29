b5abbc9329b53560e898954f00ae0195d1a7d2d2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-04-29 20:50:51 +0530

    chore added tanstack query table dependencies

---
+++ client/src/app/myfiles/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Table } from "@/components/ui/table";
+
+const myFilesPage = () => {
+    return(
+        <>
+        <Table/>
+        </>
+    )
+};
+
+export default myFilesPage;
