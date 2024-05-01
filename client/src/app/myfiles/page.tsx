960796d88edff142863fd5c3c3f79de0fb76f712
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-05-01 05:59:40 +0530

    refactor added type interface

--- client/src/app/myfiles/page.tsx
+++ client/src/app/myfiles/page.tsx
@@ -2,6 +2,28 @@
 
 import Navbar from "@/components/ui/navbar";
 import { Table } from "@/components/ui/table";
+import React, { useEffect, useMemo, useState } from 'react'
+import io from 'socket.io-client'
+import { AppDispatch, useAppSelector } from '@/redux/store';
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { logIn, logOut } from '@/redux/features/auth-slice';
+import { toast } from 'react-toastify';
+
+interface File {
+  createdAt: string;
+  filename: string;
+  fileurl: string;
+  fileType: string | null;
+  receiveremail: string;
+  senderemail: string;
+  sharedAt: string;
+  updatedAt: string;
+  _id: string;
+}
+
+let socket: any = null;
+let apiurl: string = `${process.env.NEXT_PUBLIC_URL}`
 
 const myFilesPage = () => {
   return (
