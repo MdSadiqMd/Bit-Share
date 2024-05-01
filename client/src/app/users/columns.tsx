88eb9a98602e29ff8d3e5cf6843519d6715b0b35
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-05-01 06:05:49 +0530

    refactor added coloumn schema

---
+++ client/src/app/users/columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+interface File {
+  filename: string;
+  fileurl: string;
+  fileType: string | null;
+  receiveremail: string;
+  senderemail: string;
+  sharedAt: string;
+  updatedAt: string;
+  createdAt: string;
+  _id: string;
+}
+
+export const columns: ColumnDef<File>[] = [
+  {
+    accessorKey: "filename",
+    header: "File Name",
+  },
+  {
+    accessorKey: "senderemail",
+    header: "Sender",
+  },
+  {
+    accessorKey: "fileType",
+    header: "fileType",
+  },
+  {
+    accessorKey: "sharedAt",
+    header: "sharedAt",
+  },
+  {
+    accessorKey: "fileurl",
+    header: "fileurl",
+  },
+];
